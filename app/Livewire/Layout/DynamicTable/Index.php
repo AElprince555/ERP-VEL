533df123c875970c5c2287463df72c0b97080ac3
Aelprince555 <ahmed.mahmoued@outlook.com>
2024-07-07 02:47:34 +0300

    Initial commit

---
+++ app/Livewire/Layout/DynamicTable/Index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Livewire\Layout\DynamicTable;
+
+use Livewire\Component;
+
+class Index extends Component{
+    public function render()
+    {
+        return view('livewire.layout.dynamic-table.index');
+    }
+}
